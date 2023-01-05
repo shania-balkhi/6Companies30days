bba929998398547c542df4df772f84d3727407ff
Shania Balkhi <balkhi.shania@gmail.com>
2023-01-05 23:36:45 +0530

    Create Solution13.java

---
+++ Jan1-Jan5_ Microsoft/Solution13.java
@@ -0,0 +1,16 @@
+/*Q13. Seat Arrangement in a SpiceJet Problem - Online OA February ‘22*/
+/* https://leetcode.com/problems/airplane-seat-assignment-probability/ */
+
+class Solution {
+    public double nthPersonGetsNthSeat(int n) 
+    {
+        if(n==1)
+        {
+            return 1.00000;
+        }
+        else
+        {
+            return 0.50000;
+		}
+     }
+}
