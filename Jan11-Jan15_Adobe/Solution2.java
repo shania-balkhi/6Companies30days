2dd5044d73c6a4756138bb01f0415894ff9b19cd
Shania Balkhi <balkhi.shania@gmail.com>
2023-01-11 23:34:36 +0530

    Create Solution2.java

---
+++ Jan11-Jan15_Adobe/Solution2.java
@@ -0,0 +1,14 @@
+/*Q2. Increasing Triplet Sequence*/
+/* https://leetcode.com/problems/increasing-triplet-subsequence/ */
+
+public class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int min = Integer.MAX_VALUE, secondMin = Integer.MAX_VALUE;
+        for(int num : nums){
+            if(num <= min) min = num;
+            else if(num < secondMin) secondMin = num;
+            else if(num > secondMin) return true;
+        }
+        return false;
+    }
+}
