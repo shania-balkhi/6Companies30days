f3088bb186005b8df26e0d00763096452f34048c
Shania Balkhi <balkhi.shania@gmail.com>
2023-01-20 23:05:08 +0530

    Create Solution15.java

---
+++ Jan16-Jan20_Flipkart/Solution15.java
@@ -0,0 +1,18 @@
+/*Q15. House Robber III */
+/* https://leetcode.com/problems/house-robber-iii/ */
+
+public class Solution {
+    public int rob(TreeNode root) {
+        int[] num = dfs(root);
+        return Math.max(num[0], num[1]);
+    }
+    private int[] dfs(TreeNode x) {
+        if (x == null) return new int[2];
+        int[] left = dfs(x.left);
+        int[] right = dfs(x.right);
+        int[] res = new int[2];
+        res[0] = left[1] + right[1] + x.val;
+        res[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        return res;
+    }
+}
