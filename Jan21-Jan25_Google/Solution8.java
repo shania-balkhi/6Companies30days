9d70781569f9ea226f09c3c77e67390bf32b50b1
Shania Balkhi <balkhi.shania@gmail.com>
2023-01-25 22:38:22 +0530

    Create Solution8.java

---
+++ Jan21-Jan25_Google/Solution8.java
@@ -0,0 +1,20 @@
+/*Q8. Random Pick with Weight */
+/* https://leetcode.com/problems/random-pick-with-weight/ */
+
+class Solution{
+    private final TreeMap<Integer, Integer> map;
+    private final Random random = new Random(System.currentTimeMillis());
+    private int sum = 0;
+
+    public Solution(int[] w) {
+        map = new TreeMap<>();
+        for (int i = 0; i < w.length; i++) {
+            sum += w[i];
+            map.put(sum, i);
+        }
+    }
+
+    public int pickIndex() {
+        return map.get(map.higherKey(random.nextInt(sum)));
+    }
+}
