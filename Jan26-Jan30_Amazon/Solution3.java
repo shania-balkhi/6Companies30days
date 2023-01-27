e6efb9e8e95879ba9bf42ea78e4c0006276ae8ff
Shania Balkhi <balkhi.shania@gmail.com>
2023-01-27 13:41:29 +0530

    Create Solution3.java

---
+++ Jan26-Jan30_Amazon/Solution3.java
@@ -0,0 +1,27 @@
+/*Q3. Split a String Into the Max Number of Unique Substrings*/
+/* https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings/ */
+
+
+class Solution {
+    int max = 0;
+    public int maxUniqueSplit(String s) {
+        int n = s.length();
+        backtrack(s, 0, new HashSet<String>());
+        return max;
+    }
+    public void backtrack(String s, int start, Set<String> h) {
+        if(start == s.length()) {
+            max = Math.max(max, h.size());
+        }
+        String res = "";
+        
+        for(int i = start;i < s.length();i++) {
+            res += s.charAt(i);
+            if(h.contains(res)) continue;
+            h.add(res);
+            backtrack(s, i+1, h);
+            h.remove(res);
+        }
+    }
+}
+
